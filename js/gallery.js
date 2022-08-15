8e2099dfab3c268a52664da78b06c8b0b6a292ce
Jordan Joshua <jordan.joshua@gdn-commerce.com>
2022-08-15 22:36:17 +0700

    changed youtube size in modal

--- js/gallery.js
+++ js/gallery.js
@@ -52,14 +52,6 @@ function closeModal(teamId) {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  var modal = document.getElementById("myModal");
-  if (event.target == modal) {
-    this.closeModal();
-  }
-};
-
 /* global AFRAME */
 AFRAME.registerComponent("highlight", {
   init: function () {
